924996099c805d028307fa46e8a9ddbc8e0ddbed
Ivica Kopestinskij <ivica.kopes@gmail.com>
2019-03-11 22:14:40 +0100

    Final

---
+++ README.md
@@ -0,0 +1,2 @@
+# Weather App
+A simple weather app, built following along a YouTube tutorial, using React, Express, and PostgreSQL.
---
+++ server/models/weather.js
@@ -0,0 +1,19 @@
+const request = require('request-promise');
+
+require('dotenv').config();
+
+class Weather {
+  static retrieveByCity (city, callback) {
+    request({
+      uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=87a5fdfa3b4f02f37ed34bc176792837&units=metric`,
+      json: true
+    }).then((res) => {
+      callback(res);
+    }).catch((err) => {
+      console.log(err);
+      callback({ error: 'Could not reach OpenWeatherMap API.' });
+    });
+  }
+}
+
+module.exports = Weather;
